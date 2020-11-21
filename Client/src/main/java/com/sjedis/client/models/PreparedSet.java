49b64fbceb1332c621bd78b39703c76c68af4fb7
Alexis <33222808+sathonay@users.noreply.github.com>
2020-11-21 17:03:04 +0100

    [Client] Add Connection#Set utils

---
+++ Client/src/main/java/com/sjedis/client/models/PreparedSet.java
@@ -0,0 +1,31 @@
+package com.sjedis.client.models;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PreparedSet {
+
+    private final Map<String, Object> setMap;
+
+    public PreparedSet() {
+        setMap = new HashMap<>();
+    }
+
+    public PreparedSet(Map<String, Object> map) {
+        setMap = map;
+    }
+
+    public PreparedSet set(String key, Object value) {
+        setMap.put(key, value);
+        return this;
+    }
+
+    public PreparedSet set(Multi<String> keys, Object value) {
+        keys.toList().forEach(key -> set(key, value));
+        return this;
+    }
+
+    public Map<String, Object> toMap() {
+        return setMap;
+    }
+}
